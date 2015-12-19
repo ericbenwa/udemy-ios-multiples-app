b384d2b7dd18a98288e2c5d09a476da8b705a3a6
Eric Benoit <benwa02@gmail.com>
2015-12-18 23:19:07 -0500

    Wired up Play button to hide landing page items for next screen.

--- multiples/multiples/ViewController.swift
+++ multiples/multiples/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var logo: UIImageView?
+    @IBOutlet weak var inputMultiplyNumber: UITextField?
+    @IBOutlet weak var playButton: UIButton?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playGame(sender: UIButton) {
+        logo?.hidden = true
+        inputMultiplyNumber?.hidden = true
+        playButton?.hidden = true
+    }
 
 }
 
