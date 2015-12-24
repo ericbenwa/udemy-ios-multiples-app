c12792586a4476c8fea3fe6e0bd32549b815bc3e
Eric Benoit <benwa02@gmail.com>
2015-12-24 00:55:03 -0500

    Added function to add digits.

--- multiples/multiples/ViewController.swift
+++ multiples/multiples/ViewController.swift
@@ -9,26 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    // Properties
+    var userNumber = 0
+    var addNumber = 0
+    var newSum = 0
     
-    @IBOutlet weak var logo: UIImageView?
-    @IBOutlet weak var inputMultiplyNumber: UITextField?
-    @IBOutlet weak var playButton: UIButton?
+    // First screen: Select number
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var inputMultiplyNumber: UITextField!
+    @IBOutlet weak var playButton: UIButton!
     
+    // Second screen: Add numbers
+    @IBOutlet weak var additionString: UILabel!
+    @IBOutlet weak var addButton: UIButton!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // User enters a number to launch calculator
+    @IBAction func playGamePressed(sender: UIButton!) {
+        
+        if inputMultiplyNumber.text != nil && inputMultiplyNumber.text != "" {
+            userNumber = Int(inputMultiplyNumber.text!)!
+            print("userNumber: \(userNumber)")
+            
+            logo.hidden = true
+            inputMultiplyNumber.hidden = true
+            playButton.hidden = true
+            additionString.hidden = false
+            addButton.hidden = false
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func playGame(sender: UIButton) {
-        logo?.hidden = true
-        inputMultiplyNumber?.hidden = true
-        playButton?.hidden = true
+    
+    // User requests addition of numbers
+    @IBAction func addButtonPressed(sender: UIButton!) {
+        newSum = userNumber + addNumber
+        self.additionString.text = "\(userNumber) + \(addNumber) = \(newSum)"
+        addNumber = newSum
+        print("addNumber: \(addNumber)")
+        print("newSum: \(newSum)")
     }
 
 }
